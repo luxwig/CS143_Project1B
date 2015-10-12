fdfe8b0b8cb47c7c0089e8dc124ff4e1e718ba56
Ludwig Zhao <luxwig@ucla.edu>
2015-10-11 22:42:59 -0700

    add new queries and fix minor bugs

--- queries.sql
+++ queries.sql
@@ -1,15 +1,15 @@
 -- Give me the names of all the actors in the movie 'Die Another Day'
 SELECT CONCAT(first, SPACE(1), last)
 FROM Actor A, MovieActor MA, Movie M 
-WHERE M.title = 'Die Another Day' AND 
-	MA.mid = M.id AND 
-	MA.id = A.aid;  
+WHERE (M.title = 'Die Another Day') AND 
+      (MA.mid = M.id) AND 
+      (MA.aid = A.id);  
 
 -- Give me the count of all the actors who acted in multiple movies
  
- /* Create a new schema with only the aid of the movie actors from MovieActor N, which selects actors' ids that 
- has appeared more than once, then counts the number of all these actors to get the result. 
- */
+-- Create a new schema with only the aid of the movie actors from MovieActor N, which selects actors' ids that 
+-- has appeared more than once, then counts the number of all these actors to get the result. 
+
 SELECT COUNT(*)
 FROM (SELECT COUNT(*) num
 	From MovieActor
@@ -18,3 +18,8 @@ FROM (SELECT COUNT(*) num
 WHERE num > 1; 
 
 -- another query that you came up with
+-- Show all the movies that the director is also actor
+SELECT M.* FROM Movie M, 
+                ((SELECT DISTINCT D.mid id FROM MovieDirector D, MovieActor A
+                                          WHERE A.aid = D.did AND A.mid = D.mid) N )
+           WHERE M.id = N.id;
